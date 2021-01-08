73c2ddf5d816997b6522aa1b96adf4b0f1fc619d
GongJiangHua <1914805653@qq.com>
2021-01-08 11:32:02 +0800

    0108Agroup's commad

--- controllers/getblockcountControllers.go
+++ controllers/getblockcountControllers.go
@@ -11,7 +11,7 @@ type Four_GetBlockCount struct {
 }
 
 func (t *Four_GetBlockCount) Get (){
-	t.TplName = "getdifficulty.html"
+	t.TplName = "getblockcount.html"
 	blockcount ,err := btcService.GetBlockCount()
 	if err != nil {
 		fmt.Println(err.Error())
